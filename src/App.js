c5bdea9291f05992b266c55813955a7a2d738537
Sheena Barnes <shba2504@gmail.com>
2023-12-26 14:28:05 -0700

    import statements and images folder

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
