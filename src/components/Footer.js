0e92bafda26410edcd64425d20c52172887de359
Sheena Barnes <shba2504@gmail.com>
2023-12-26 22:02:10 -0700

    more css styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,41 +2,45 @@ function Footer() {
   return (
     <footer>
       <img src="" alt="" />
-      <section>
-        <h4>Doormat Navigation</h4>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Menu</a>
-          </li>
-          <li>
-            <a href="">Reservations</a>
-          </li>
-          <li>
-            <a href="">Order Online</a>
-          </li>
-          <li>
-            <a href="">Log In</a>
-          </li>
-        </ul>
-      </section>
-      <section>
-        <h4>Contact</h4>
-        <ul>
-          <li>Address</li>
-          <li>Phone Number</li>
-          <li>Email</li>
-        </ul>
-      </section>
-      <section>
-        <h4>Social Media Links</h4>
-        <li></li>
-      </section>
+      <div className="footer">
+        <section>
+          <h4>Doormat Navigation</h4>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Menu</a>
+            </li>
+            <li>
+              <a href="">Reservations</a>
+            </li>
+            <li>
+              <a href="">Order Online</a>
+            </li>
+            <li>
+              <a href="">Log In</a>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h4>Contact</h4>
+          <ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </section>
+        <section>
+          <h4>Social Media Links</h4>
+          <ul>
+            <li></li>
+          </ul>
+        </section>
+      </div>
     </footer>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,48 +12,54 @@ function Main() {
             traditional recipes served with a modern twist.
           </p>
           <button>Reserve a Table</button>
-          <img />
         </article>
+        <img />
       </div>
       <div className="highlights">
         <article>
-          <section>
-            <h2>This Week's Specials!</h2>
-            <button>Online Menu</button>
-          </section>
-          <section>
-            <img src="" alt="Greek salad with fork" />
-            <h4>Greek Salad $12.99</h4>
-            <p>
-              The famous Greek salad of crispy lettuce, peppers, olives, and our
-              Chicago-style feta cheese, garnished with crunchy garlic and
-              rosemary croutons.
-            </p>
-            <a>Order a delivery</a>
-          </section>
-          <section>
-            <img />
-            <h4>Bruschetta $5.99</h4>
-            <p>
-              Our Bruschetta is made from grilled bread that has been smeared
-              with garlic and seasoned with salt and olive oil.
-            </p>
-            <a>Order a delivery</a>
-          </section>
-          <section>
-            <img />
-            <h4>Lemon Dessert $5.00</h4>
-            <p>
-              This comes straight from grandma's recipe book, every last
-              ingredient has been sourced and is as authentic as can be
-              imagined.
-            </p>
-            <a>Order a delivery</a>
-          </section>
+          <div className="container1">
+            <section>
+              <h2>This Week's Specials!</h2>
+            </section>
+            <section>
+              <button>Online Menu</button>
+            </section>
+          </div>
+          <div className="container2">
+            <section className="item">
+              <h4>Greek Salad $12.99</h4>
+              <p>
+                The famous Greek salad of crispy lettuce, peppers, olives, and
+                our Chicago-style feta cheese, garnished with crunchy garlic and
+                rosemary croutons.
+              </p>
+              <a>Order delivery</a>
+            </section>
+            <section className="item">
+              <img />
+              <h4>Bruschetta $5.99</h4>
+              <p>
+                Our Bruschetta is made from grilled bread that has been smeared
+                with garlic and seasoned with salt and olive oil.
+              </p>
+              <a>Order delivery</a>
+            </section>
+            <section className="item">
+              <img />
+              <h4>Lemon Dessert $5.00</h4>
+              <p>
+                This comes straight from grandma's recipe book, every last
+                ingredient has been sourced and is as authentic as can be
+                imagined.
+              </p>
+              <a>Order delivery</a>
+            </section>
+          </div>
         </article>
       </div>
       <div className="testimonials">
         <article>
+          <h2>Testimonials</h2>
           <section></section>
           <section></section>
           <section></section>
