dfab2a0c36e4dc2c098e1720fc7d6b381b83e510
Sheena Barnes <shba2504@gmail.com>
2024-01-02 15:36:27 -0700

    adjusted responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
+import React, { useState } from "react";
 import Logo from "../icons/Logo.svg";
 import menu from "../icons/menu.svg";
+import Dish from "../icons/Dish.svg";
 import { Link } from "react-router-dom";
 
-function Nav() {
+const Nav = () => {
+  const [toggleMenu, setToggleMenu] = useState(true);
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img src={Logo} alt="Little Lemon logo" />
+        <img src={Logo} alt="Little Lemon restaurant logo" />
       </div>
-      <ul className="nav-links">
+      <ul className="navbar-links">
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -28,8 +31,43 @@ function Nav() {
           <a href="#">Log In</a>
         </li>
       </ul>
+      <div className="navbar-mobile">
+        <img
+          src={menu}
+          alt="hamburger menu"
+          onClick={() => {
+            setToggleMenu(false);
+          }}
+        />
+        <img
+          src={Dish}
+          className="overlay-close"
+          alt="serving platter icon"
+          onClick={() => {}}
+        />
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/menu">Menu</Link>
+          </li>
+          <li>
+            <Link to="/reservations">Reservations</Link>
+          </li>
+          <li>
+            <a href="#">Order Online</a>
+          </li>
+          <li>
+            <a href="#">Log In</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
-}
+};
 
 export default Nav;
