86f47cd5ed9c033b435573e3bf4310b2701172e7
Sheena Barnes <shba2504@gmail.com>
2024-01-02 20:27:10 -0700

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import Dish from "../icons/Dish.svg";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-  const [toggleMenu, setToggleMenu] = useState(true);
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -31,40 +31,43 @@ const Nav = () => {
           <a href="#">Log In</a>
         </li>
       </ul>
-      <div className="navbar-mobile">
+      <div className="navbar-mobile_overlay">
         <img
           src={menu}
           alt="hamburger menu"
-          onClick={() => {
-            setToggleMenu(false);
-          }}
+          onClick={() => setToggleMenu(true)}
         />
-        <img
-          src={Dish}
-          className="overlay-close"
-          alt="serving platter icon"
-          onClick={() => {}}
-        />
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/reservations">Reservations</Link>
-          </li>
-          <li>
-            <a href="#">Order Online</a>
-          </li>
-          <li>
-            <a href="#">Log In</a>
-          </li>
-        </ul>
+
+        {toggleMenu && (
+          <div>
+            <img
+              src={Dish}
+              className="overlay_close"
+              alt="serving platter icon"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="navbar-mobile_links">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/menu">Menu</Link>
+              </li>
+              <li>
+                <Link to="/reservations">Reservations</Link>
+              </li>
+              <li>
+                <a href="#">Order Online</a>
+              </li>
+              <li>
+                <a href="#">Log In</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
